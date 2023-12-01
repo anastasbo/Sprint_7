9729e149bdef8c65793471dfeae074c3884ba836
Anastasia Bond <bag1508@yandex.ru>
2023-12-01 21:35:44 +0300

    коммит после ревью

--- src/test/java/tests/courier/CreateCourierTest.java
+++ src/test/java/tests/courier/CreateCourierTest.java
@@ -7,18 +7,37 @@ import io.qameta.allure.junit4.DisplayName;
 import io.restassured.response.ValidatableResponse;
 import org.apache.commons.lang3.RandomStringUtils;
 import org.hamcrest.Matchers;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 public class CreateCourierTest {
+
+    private CourierClient clientStep;
+    private String login;
+    private String password;
+    private String firstName;
+
+    @Before
+    public void setUp() {
+        clientStep = new CourierClient();
+        login = RandomStringUtils.randomAlphanumeric(1,10);
+        password = RandomStringUtils.randomAlphanumeric(6,8);
+        firstName = RandomStringUtils.randomAlphabetic(3,10);
+    }
+
+    @After
+    public void tearDown() {
+        clientStep = null;
+        login = null;
+        password = null;
+        firstName = null;
+    }
 
     @Test
     @DisplayName("Successful create courier.")
     @Description("Check status code and field value for /api/v1/courier(successful request).")
     public void createCourierTest(){
-        CourierClient clientStep = new CourierClient();
-        String login = RandomStringUtils.randomAlphanumeric(1,10);
-        String password = RandomStringUtils.randomAlphanumeric(6,8);
-        String firstName = RandomStringUtils.randomAlphabetic(3,10);
         ValidatableResponse response = clientStep.getCourierApiV1Response(new Courier(login, password, firstName));
         response.statusCode(201).and().assertThat().body("ok", Matchers.is(true));
 
@@ -28,10 +47,6 @@ public class CreateCourierTest {
     @DisplayName("Create two identical couriers.")
     @Description("Check status code and message existence when create two identical couriers.")
     public void createTwoIdenticalCouriersTest(){
-        CourierClient clientStep = new CourierClient();
-        String login = RandomStringUtils.randomAlphanumeric(1,10);
-        String password = RandomStringUtils.randomAlphanumeric(6,8);
-        String firstName = RandomStringUtils.randomAlphabetic(3,10);
         Courier courier = new Courier(login, password, firstName);
         clientStep.getCourierApiV1Response(courier);
         ValidatableResponse response = clientStep.getCourierApiV1Response(courier);
@@ -42,10 +57,6 @@ public class CreateCourierTest {
     @DisplayName("Create two couriers with identical logins.")
     @Description("Check status code and message existence when create two couriers with identical logins.")
     public void createTwoIdenticalLoginTest(){
-        CourierClient clientStep = new CourierClient();
-        String login = RandomStringUtils.randomAlphanumeric(1,10);
-        String password = RandomStringUtils.randomAlphanumeric(6,8);
-        String firstName = RandomStringUtils.randomAlphabetic(3,10);
         Courier courier = new Courier(login, password, firstName);
         clientStep.getCourierApiV1Response(courier);
         courier.setPassword("abracadabra");
@@ -57,8 +68,6 @@ public class CreateCourierTest {
     @DisplayName("Create courier without login and password.")
     @Description("Check status code and message existence when create courier without login and password(Bad request).")
     public void createCourierWithoutLoginAndPassword(){
-        CourierClient clientStep = new CourierClient();
-        String firstName = RandomStringUtils.randomAlphabetic(3,10);
         Courier courier = new Courier();
         courier.setFirstName(firstName);
         ValidatableResponse response = clientStep.getCourierApiV1Response(courier);
@@ -69,8 +78,6 @@ public class CreateCourierTest {
     @DisplayName("Create courier without login and first name.")
     @Description("Check status code and message existence in create courier without login and first name(Bad request).")
     public void createCourierWithoutLoginAndFirstName(){
-        CourierClient clientStep = new CourierClient();
-        String password = RandomStringUtils.randomAlphabetic(6,8);
         Courier courier = new Courier();
         courier.setPassword(password);
         ValidatableResponse response = clientStep.getCourierApiV1Response(courier);
@@ -81,9 +88,8 @@ public class CreateCourierTest {
     @DisplayName("Create courier without password and first name.")
     @Description("Check status code and message existence in create courier without password and first name(Bad request).")
     public void createCourierWithoutPasswordAndFirstName(){
-        CourierClient clientStep = new CourierClient();
         Courier courier = new Courier();
-        courier.setLogin(RandomStringUtils.randomAlphabetic(3,10));
+        courier.setLogin(login);
         ValidatableResponse response = clientStep.getCourierApiV1Response(courier);
         response.statusCode(400).and().assertThat().body("message", Matchers.notNullValue());
     }
@@ -94,8 +100,8 @@ public class CreateCourierTest {
     public void createCourierWithoutPassword(){
         CourierClient clientStep = new CourierClient();
         Courier courier = new Courier();
-        courier.setLogin(RandomStringUtils.randomAlphabetic(1,10));
-        courier.setFirstName(RandomStringUtils.randomAlphabetic(3,10));
+        courier.setLogin(login);
+        courier.setFirstName(firstName);
         ValidatableResponse response = clientStep.getCourierApiV1Response(courier);
         response.statusCode(400).and().assertThat().body("message", Matchers.notNullValue());
     }
@@ -104,10 +110,9 @@ public class CreateCourierTest {
     @DisplayName("Create courier without login.")
     @Description("Check status code and message existence in create courier without login(Bad request).")
     public void createCourierWithoutLogin(){
-        CourierClient clientStep = new CourierClient();
         Courier courier = new Courier();
-        courier.setFirstName(RandomStringUtils.randomAlphabetic(3,10));
-        courier.setPassword(RandomStringUtils.randomAlphanumeric(6,8));
+        courier.setFirstName(firstName);
+        courier.setPassword(password);
         ValidatableResponse response = clientStep.getCourierApiV1Response(courier);
         response.statusCode(400).and().assertThat().body("message", Matchers.notNullValue());
     }
